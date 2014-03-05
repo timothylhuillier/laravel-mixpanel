90d85902ed1a09095bdfac4ce0ac8a685c3d580d
Timothy L'HUILLIER <timothy.lhuillier@gmail.com>
2014-03-05 17:17:03 +0100

    LaravelMixpanel : ajoute 'getInstance()'

--- src/Timothylhuillier/LaravelMixpanel/LaravelMixpanel.php
+++ src/Timothylhuillier/LaravelMixpanel/LaravelMixpanel.php
@@ -1,16 +1,36 @@
 <?php namespace Timothylhuillier\LaravelMixpanel;
 
 use \Mixpanel;
+use Config;
 
 class LaravelMixpanel extends Mixpanel {
 
     protected $token;
 
+    private static $_instance;
+
     public function __construct($token, $options = array())
     {
         $this->setToken($token);
 
-        return parent::__construct($this->token, $options = array());
+        self::$_instance = parent::__construct($this->token, $options = array());
+
+        return self::$_instance;
+    }
+
+    public static function getInstance($token = false, $options = array())
+    {
+        if (!$token)
+        {
+            $token = Config::get('laravel-mixpanel::token');
+        }
+
+        if (!isset(self::$_instance))
+        {
+            self::$_instance = new LaravelMixpanel($token, $options);
+        }
+
+        return self::$_instance;
     }
 
     public function setToken($token)
